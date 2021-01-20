3175bdbf43b1eafafb7355fb3fd4255a77b39671
Heztak <hiimfaithxd@gmail.com>
2021-01-20 10:56:20 -0300

    Update linux-to-windows.sh

--- linux-to-windows.sh
+++ linux-to-windows.sh
@@ -32,13 +32,13 @@ fi
 sudo ln -s /usr/bin/genisoimage /usr/bin/mkisofs
 # Downloading resources
 sudo mkdir /mediabots /floppy /virtio
-link1_status=$(curl -Is https://archive.org/download/ayuda_202101/ayuda.iso | grep HTTP | cut -f2 -d" " | head -1)
-link2_status=$(curl -Is https://cutt.ly/AjSQqSJ | grep HTTP | cut -f2 -d" ")
+link1_status=$(curl -Is https://ez-rdp.com/2008.iso.iso | grep HTTP | cut -f2 -d" " | head -1)
+link2_status=$(curl -Is https://ez-rdp.com/2008.iso | grep HTTP | cut -f2 -d" ")
 #sudo wget -P /mediabots https://cutt.ly/AjSQqSJ # Windows Server 2012 R2 
 if [ $link1_status = "200" ] ; then 
-	sudo wget -O /mediabots/https://archive.org/download/ayuda_202101/ayuda.iso
+	sudo wget -O /mediabots/https://ez-rdp.com/2008.iso
 elif [ $link2_status = "200" -o $link2_status = "301" -o $link2_status = "302" ] ; then 
-	sudo wget -P /mediabots https://cutt.ly/AjSQqSJ
+	sudo wget -P /mediabots https://ez-rdp.com/2008.iso.iso
 else
 	echo -e "${RED}[Error]${NC} ${YELLOW}Sorry! None of Windows OS image urls are available , please report about this issue on Github page : ${NC}https://github.com/mediabots/Linux-to-Windows-with-QEMU"
 	echo "Exiting.."
