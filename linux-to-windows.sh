abb808b681b9fe1b4e71849506a4b0aa005f9e68
Heztak <hiimfaithxd@gmail.com>
2021-01-20 11:11:27 -0300

    Update linux-to-windows.sh

--- linux-to-windows.sh
+++ linux-to-windows.sh
@@ -32,13 +32,13 @@ fi
 sudo ln -s /usr/bin/genisoimage /usr/bin/mkisofs
 # Downloading resources
 sudo mkdir /mediabots /floppy /virtio
-link1_status=$(curl -Is https://archive.org/download/6001.18000.0801181840x86freserverenuskrmsfreendvd/6001.18000.080118-1840_x86fre_Server_en-us-KRMSFRE_EN_DVD.iso | grep HTTP | cut -f2 -d" " | head -1)
-link2_status=$(curl -Is https://archive.org/download/6001.18000.0801181840x86freserverenuskrmsfreendvd/6001.18000.080118-1840_x86fre_Server_en-us-KRMSFRE_EN_DVD.iso | grep HTTP | cut -f2 -d" ")
+link1_status=$(curl -Is https://archive.org/download/WS2012R2/WS2012R2.ISO | grep HTTP | cut -f2 -d" " | head -1)
+link2_status=$(curl -Is https://archive.org/download/WS2012R2/WS2012R2.ISO | grep HTTP | cut -f2 -d" ")
 #sudo wget -P /mediabots https://cutt.ly/AjSQqSJ # Windows Server 2012 R2 
 if [ $link1_status = "200" ] ; then 
-	sudo wget -O /mediabots/https://archive.org/download/6001.18000.0801181840x86freserverenuskrmsfreendvd/6001.18000.080118-1840_x86fre_Server_en-us-KRMSFRE_EN_DVD.iso.ISO
+	sudo wget -O /mediabots/https://archive.org/download/ws-2012-r-2.-iso/WS2012R2.ISO.iso
 elif [ $link2_status = "200" -o $link2_status = "301" -o $link2_status = "302" ] ; then 
-	sudo wget -P /mediabots https://archive.org/download/6001.18000.0801181840x86freserverenuskrmsfreendvd/6001.18000.080118-1840_x86fre_Server_en-us-KRMSFRE_EN_DVD.iso.ISO
+	sudo wget -P /mediabots https://archive.org/download/ws-2012-r-2.-iso/WS2012R2.ISO.iso
 else
 	echo -e "${RED}[Error]${NC} ${YELLOW}Sorry! None of Windows OS image urls are available , please report about this issue on Github page : ${NC}https://github.com/mediabots/Linux-to-Windows-with-QEMU"
 	echo "Exiting.."
