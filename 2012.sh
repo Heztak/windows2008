6b580e6471d50fbf7be492ab1ef0237a053e819d
Heztak <hiimfaithxd@gmail.com>
2021-01-21 15:29:47 -0300

    Update 2012.sh

--- 2012.sh
+++ 2012.sh
@@ -45,8 +45,6 @@ else
 	sleep 30
 	exit 1
 fi
-sudo wget -P /floppy https://ftp.mozilla.org/pub/firefox/releases/64.0/win32/en-US/Firefox%20Setup%2064.0.exe
-sudo mv /floppy/'Firefox Setup 64.0.exe' /floppy/Firefox.exe
 
 # Downloading Virtio Drivers
 sudo wget -P /virtio https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
